d10a557ec8750afb8befaa1ead916a99d9ca68bd
sesang06 <sesang06@naver.com>
2018-12-28 21:48:05 +0900

    filter 액티비티 생성

--- src/app/src/main/java/com/sesang06/wa8upandroid/MainActivity.kt
+++ src/app/src/main/java/com/sesang06/wa8upandroid/MainActivity.kt
@@ -2,11 +2,13 @@ package com.sesang06.wa8upandroid
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        textview_main_title.text = "hello wo?"
     }
 }
